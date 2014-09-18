a872a9f8c61ac72940b7e2e72c690bbe47d12bf4
Gustavo Robles <gustavo.robles@crowdint.com>
2014-09-17 22:15:39 -0500

    post shipment to newgistics after order is completed

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,6 +1,6 @@
 Spree::Order.class_eval do
 
-  state_machine.after_transition to: :complete, do: :post_to_newgistics
+  after_touch :post_to_newgistics, if: :completed_at?
 
 
   def add_newgistics_shipment_content(sku, qty)
