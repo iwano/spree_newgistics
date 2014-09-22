734f9d5137272415d7af06e10232ec96b5ecb4fb
Gustavo Robles <gustavo.robles@crowdint.com>
2014-09-22 18:53:43 -0500

    finished inventory puller

--- app/async/workers/inventory_puller.rb
+++ app/async/workers/inventory_puller.rb
@@ -3,6 +3,29 @@ module Workers
     include Sidekiq::Worker
 
     def perform
+
+      response = Spree::Newgistics::HTTPManager.get('inventory.aspx')
+      if response.status == 200
+        xml = Nokogiri::XML(response.body).to_xml
+        stock_items = Hash.from_xml(xml)["response"]["products"]
+        if stock_items
+          update_inventory(stock_items.values.flatten)
+        end
+      end
+    end
+
+    def update_inventory(newgistics_stock_items)
+      newgistics_stock_items.each do |newgistic_stock_item|
+        variant = Spree::Variant.find_by(sku: newgistic_stock_item["sku"])
+        next unless variant
+        ## Since newgistics is the only stock location, set 1 as stock_location id.
+        ## TODO: add support for multiple stock locations.
+        stock_item = variant.stock_items.find_by(stock_location_id: 1)
+        if stock_item
+          stock_item.update_column(:count_on_hold, newgistic_stock_item['pendingQuantity'].to_i)
+          stock_item.update_column(:count_on_hand, newgistic_stock_item['availableQuantity'].to_i)
+        end
+      end
     end
   end
 end
