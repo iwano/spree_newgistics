40045d5e33fbbfff9ed61be60fba5570d40e6a9f
Gustavo Robles <gustavo.robles@crowdint.com>
2014-09-24 12:39:17 -0500

    use only one pair of env variables for config

--- lib/spree/newgistics/configuration.rb
+++ lib/spree/newgistics/configuration.rb
@@ -1,12 +1,8 @@
 module Spree
   module Newgistics
     class Configuration < Spree::Preferences::Configuration
-      preference :api_key, :string, default: ENV['NEWGISTICS_STAGING_API_KEY']
-      preference :url, :string, default: ENV['NEWGISTICS_STAGING_URL']
-      preference :staging_api_key, :string, default: ENV['NEWGISTICS_STAGING_API_KEY']
-      preference :staging_url, :string, default: ENV['NEWGISTICS_STAGING_URL']
-      preference :production_api_key, :string, default: ENV['NEWGISTICS_PRODUCTION_API_KEY']
-      preference :production_url, :string, default: ENV['NEWGISTICS_PRODUCTION_URL']
+      preference :api_key, :string, default: ENV['NEWGISTICS_API_KEY']
+      preference :url, :string, default: ENV['NEWGISTICS_URL']
     end
   end
 end
