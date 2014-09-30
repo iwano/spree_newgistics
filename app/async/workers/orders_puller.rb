0e0cbcc0c09339c0dbca9a915d2851a08e894cc7
Gustavo Robles <gustavo.robles@crowdint.com>
2014-09-30 11:32:21 -0500

    change spree order state if newgistics state is shipped

--- app/async/workers/orders_puller.rb
+++ app/async/workers/orders_puller.rb
@@ -21,28 +21,29 @@ module Workers
     def update_shipments shipments
       shipments.each do |shipment|
         order = Spree::Order.find_by(number: shipment['OrderID'])
-        state_id = Spree::State.find_by(abbr: shipment['State']).try(:id)
-        country_id = Spree::Country.find_by(iso_name: shipment['Country']).try(:id)
-        attributes = {
-            newgistics_status: shipment['ShipmentStatus'],
-            ship_address_attributes: {
-              firstname: shipment['FirstName'],
-              lastname: shipment['LastName'],
-              company: shipment['Company'],
-              address1: shipment['Address1'],
-              address2: shipment['Address2'],
-              city: shipment['City'],
-              zipcode: shipment['PostalCode'],
-              phone: shipment['Phone']
-            }
-        }
+        if order
+          state_id = Spree::State.find_by(abbr: shipment['State']).try(:id)
+          country_id = Spree::Country.find_by(iso_name: shipment['Country']).try(:id)
+          attributes = {
+              newgistics_status: shipment['ShipmentStatus'],
+              ship_address_attributes: {
+                firstname: shipment['FirstName'],
+                lastname: shipment['LastName'],
+                company: shipment['Company'],
+                address1: shipment['Address1'],
+                address2: shipment['Address2'],
+                city: shipment['City'],
+                zipcode: shipment['PostalCode'],
+                phone: shipment['Phone']
+              }
+          }
 
-        attributes[:ship_address_attributes].merge!({state_id: state_id}) if state_id
-        attributes[:ship_address_attributes].merge!({country_id: country_id}) if country_id
-
-        order.assign_attributes(attributes)
-        order.save! if order.changed?
-
+          attributes[:ship_address_attributes].merge!({state_id: state_id}) if state_id
+          attributes[:ship_address_attributes].merge!({country_id: country_id}) if country_id
+          order.assign_attributes(attributes)
+          order.shipments.each{ |shipment| shipment.ship! } if order.newgistics_status == 'SHIPPED' && !order.shipped?
+          order.save! if order.changed?
+        end
       end
     end
 
