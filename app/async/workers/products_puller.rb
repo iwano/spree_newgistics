067e9b8c045f100e307f165419bb14cc22f10a11
Gustavo Robles <gustavo.robles@crowdint.com>
2014-10-16 18:30:52 -0500

    next if product is inactive

--- app/async/workers/products_puller.rb
+++ app/async/workers/products_puller.rb
@@ -19,6 +19,8 @@ module Workers
       disable_callbacks
       products.each do |product|
 
+        next if product['isActive'] == 'false'
+
         begin
           spree_variant = Spree::Variant.find_by(sku: product['sku'])
 
