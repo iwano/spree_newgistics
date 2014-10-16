2fc5ffc28330217f2392090b7745b8abb7f5ca80
Gustavo Robles <gustavo.robles@crowdint.com>
2014-10-16 18:27:53 -0500

    add support to update skus from newgistics

--- app/async/workers/products_puller.rb
+++ app/async/workers/products_puller.rb
@@ -18,78 +18,63 @@ module Workers
       log = File.open("#{Rails.root}/log/missing_newgistics_products.log", 'w')
       disable_callbacks
       products.each do |product|
-          color_code = product['sku'].match(/-([^-]*)$/).try(:[],1).to_s
-
-        ## keep moving if variant already exists
-        next if Spree::Variant.find_by(sku: product['sku'])
 
         begin
+          spree_variant = Spree::Variant.find_by(sku: product['sku'])
 
-          item_category_id = product["category"].present? ? Spree::ItemCategory.find_or_create_by!(name: product["category"].downcase.camelcase).id : nil
+          if spree_variant
 
-          ## if sku has color code it means we need to build
-          if color_code.present?
-            ## build a master variant sku
-            product_code = product['sku'].match(/^(.*)-/)[1].to_s
-            master_variant_sku = "#{product_code}-00"
-            master_variant = Spree::Variant.find_by(sku: master_variant_sku)
-
-            ## if we already have a master variant it means a product has been created
-            ## let's just add a new variant to the product.
-            ## else create a new product, let spree callbacks create the master variant
-            ## and change the sku to the one we want.
-            if master_variant
-              puts '*'*100
-              puts "master variant found, adding variants to product #{master_variant.product.id}"
-              variant = master_variant.product.variants.new(get_attributes_from(product))
-              variant.assign_attributes({ item_category_id: item_category_id,
-                                           posted_to_newgistics: true,
-                                           vendor_sku: product['supplierCode'],
-                                           vendor: product['supplier'],
-                                           newgistics_active: product['isActive'] == 'true' ? true : false
-                                         })
-              variant.save!
-            else
-              puts '*'*100
-              puts "no master variant found, creating new product and asigning a new variant, setting the master varianr #{master_variant_sku}"
-              spree_product = Spree::Product.new(get_attributes_from(product))
-              spree_product.taxons << supplier_from(product) if product['supplier'].present?
-              spree_product.master.assign_attributes({ posted_to_newgistics: true,
-                                                       item_category_id: item_category_id,
-                                                       upc: spree_product.upc,
-                                                       sku: master_variant_sku,
-                                                       vendor_sku: product['supplierCode'],
-                                                       vendor: product['supplier'],
-                                                       newgistics_active: product['isActive'] == 'true' ? true : false})
-
-              spree_variant = Spree::Variant.new(get_attributes_from(product))
-              spree_variant.assign_attributes({posted_to_newgistics: true,
-                                               item_category_id: item_category_id,
-                                               upc: spree_product.upc,
+            spree_variant.update_attributes!({ upc: product['upc'],
+                                               cost_price: product['value'].to_f,
+                                               price: product['retailValue'].to_f,
+                                               height: product['height'].to_f,
+                                               width: product['width'].to_f,
+                                               weight: product['weight'].to_f,
+                                               depth: product['depth'].to_f,
                                                vendor_sku: product['supplierCode'],
                                                vendor: product['supplier'],
-                                               newgistics_active: product['isActive'] == 'true' ? true : false})
-              spree_variant.save!
-
-              spree_product.variants << spree_variant
-              spree_product.save!
-            end
-
+                                               newgistics_active: product['isActive'] == 'true' ? true : false })
           else
-            puts '*'*100
-            puts "creating a new product without variants for #{product["sku"]}"
-            spree_product = Spree::Product.new(get_attributes_from(product))
-            spree_product.taxons << supplier_from(product) if product['supplier'].present?
-            spree_product.save!
-            master = spree_product.master
-            master.update_attributes!({posted_to_newgistics: true,
-                                       item_category_id: item_category_id,
-                                       upc: product['upc'],
-                                       vendor_sku: product['supplierCode'],
-                                       vendor: product['supplier'],
-                                       newgistics_active: product['isActive'] == 'true' ? true : false})
-          end
 
+            color_code = product['sku'].match(/-([^-]*)$/).try(:[],1).to_s
+
+            ## if sku has color code it means we need to build and group variants together
+            if color_code.present?
+
+              ## build a master variant sku which would be the same color code with 0000
+              product_code = product['sku'].match(/^(.*)-/)[1].to_s
+              master_variant_sku = "#{product_code}-00"
+              master_variant = Spree::Variant.find_by(sku: master_variant_sku)
+
+              ## if we already have a master variant it means a product has been created
+              ## let's just add a new variant to the product.
+              ## else create a new product, let spree callbacks create the master variant
+              ## and change the sku to the one we want.
+              if master_variant
+                variant = master_variant.product.variants.new(get_attributes_from(product))
+                variant.assign_attributes(variant_attributes_from(product))
+                variant.save!
+              else
+                spree_product = Spree::Product.new(get_attributes_from(product))
+                spree_product.taxons << supplier_from(product) if product['supplier'].present?
+                spree_product.master.assign_attributes(variant_attributes_from(product).merge({ sku: master_variant_sku }))
+
+                spree_variant = Spree::Variant.new(get_attributes_from(product))
+                spree_variant.assign_attributes(variant_attributes_from(product))
+                spree_variant.save!
+
+                spree_product.variants << spree_variant
+                spree_product.save!
+              end
+
+            else
+              spree_product = Spree::Product.new(get_attributes_from(product))
+              spree_product.taxons << supplier_from(product) if product['supplier'].present?
+              spree_product.save!
+              master = spree_product.master
+              master.update_attributes!(variant_attributes_from(product))
+            end
+          end
         rescue StandardError => e
           log.write("#{product['sku']} = #{e.message}\n")
         end
@@ -129,12 +114,25 @@ module Workers
       Spree::Product.set_callback(:save, :after, :enqueue_for_reindex)
     end
 
+    def variant_attributes_from(product)
+      item_category_id = product["category"].present? ? Spree::ItemCategory.find_or_create_by!(name: product["category"].downcase.camelcase).id : nil
+
+      {
+          posted_to_newgistics: true,
+          item_category_id: item_category_id,
+          upc: product['upc'],
+          vendor_sku: product['supplierCode'],
+          vendor: product['supplier'],
+          newgistics_active: product['isActive'] == 'true' ? true : false
+      }
+    end
+
     def get_attributes_from(product)
       {
           sku: product['sku'],
           name: product['description'],
           description: product['description'],
-          slug: product['description'].present? ? product['description'].split(' ').join('-') : '',
+          slug: product['description'].present? ? product['description'].downcase.split(' ').join('-') : '',
           upc: product['upc'],
           cost_price: product['value'].to_f,
           price: product['retailValue'].to_f,
