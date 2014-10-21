62b7b5a30b62b2356aa7cc83a36c54d006c965a1
Gustavo Robles <gustavo.robles@crowdint.com>
2014-10-21 16:55:14 -0700

    dont post skus to newgistics if they're master

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,20 +1,19 @@
 Spree::Variant.class_eval do
 
-  after_create :post_to_newgistics
-  after_update :post_to_newgistics
+  after_create :post_to_newgistics, unless: lambda { |v| v.is_master? }
+  after_update :post_to_newgistics, unless: lambda { |v| v.is_master? }
 
-  scope :not_in_newgistics, -> { where(posted_to_newgistics: false) }
+  scope :not_in_newgistics, -> { where(posted_to_newgistics: false, is_master: false) }
 
   ## This method posts the new variant to newgistics, if it success, it updates the
   ## posted_to_newgistics flag in the variant for further queue updates control.
   def post_to_newgistics
-    document = Spree::Newgistics::DocumentBuilder.build_product([self])
-    response = Spree::Newgistics::HTTPManager.post('/post_products.aspx', document)
+      document = Spree::Newgistics::DocumentBuilder.build_product([self])
+      response = Spree::Newgistics::HTTPManager.post('/post_products.aspx', document)
 
-    if response.status == 200
-      errors = Nokogiri::XML(response.body).css('errors').children.any?
-      update_column(:posted_to_newgistics, true) unless errors
-    end
-
+      if response.status == 200
+        errors = Nokogiri::XML(response.body).css('errors').children.any?
+        update_column(:posted_to_newgistics, true) unless errors
+      end
   end
 end
