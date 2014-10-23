bb33a290773bdb1994de7021deb6cb8aca0d1004
Gustavo Robles <gustavo.robles@crowdint.com>
2014-10-23 16:32:11 -0700

    fix sidekiq schedule location

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,2 +1,2 @@
 require 'sidekiq/scheduler'
-Sidekiq.schedule = YAML.load_file(File.expand_path("../../../config/sidekiq-schedule.yml",__FILE__)) if Rails.env.production?
+Sidekiq.schedule = YAML.load_file(File.expand_path("../../config/sidekiq-schedule.yml",__FILE__)) if Rails.env.production?
